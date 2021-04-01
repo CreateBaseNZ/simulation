81091f27e9dd281e2397b95f3ccfca1d09d5e3dc
Craig Vaz <craig.vaz98@gmail.com>
2021-04-01 20:57:53 +1300

    Fix webpack warnings

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,8 @@ module.exports = {
     devServer: {
         host: "0.0.0.0",
         port: 8000, //port that we're using for local host (localhost:8000)
-        disableHostCheck: true,
         contentBase: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
         publicPath: "/",
-        hot: true,
     },
     module: {
         rules: [
@@ -41,6 +39,11 @@ module.exports = {
             {
                 test: /\.ttf$/,
                 use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
             },
         ],
     },
